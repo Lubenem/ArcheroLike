654c22879c1e247c98e342085ec0d92026ce6795
Lubenem <lubenemkrkm@gmail.com>
2020-05-14 22:42:11 +0300

    -

--- Assets/Scripts/Characters/Mover.cs
+++ Assets/Scripts/Characters/Mover.cs
@@ -1,18 +1,42 @@
+using System;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    private Vector3 keyboardVel;
-    private Vector3 joystickVel;
     [HideInInspector] public Vector3 velocity;
     public Joystick joystick;
     public float speed = 3;
 
     private void Update()
     {
-        keyboardVel = new Vector3(Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal"));
-        joystickVel = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
-        velocity = keyboardVel.normalized + joystickVel;
-        GetComponent<Rigidbody>().AddForce(velocity * speed, ForceMode.Impulse);
+        velocity = new Vector3(joystick.Horizontal, 0, joystick.Vertical) * -1;
+
+        if (velocity != Vector3.zero)
+        {
+            transform.LookAt(transform.position + velocity);
+            GetComponent<Animator>().SetBool("Moving", true);
+        }
+        else
+        {
+            transform.LookAt(GetNearestEnemy());
+            GetComponent<Animator>().SetBool("Moving", false);
+        }
+    }
+
+    private Transform GetNearestEnemy()
+    {
+        float minDist = Mathf.Infinity;
+        GameObject target = null;
+
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            float dist = Vector3.Distance(transform.position, enemy.transform.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                target = enemy;
+            }
+        }
+        return target.transform;
     }
 }
